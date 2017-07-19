f8039b56b7a4df6330771e84281bc5d871767611
Luis Parada <luisparadawagner@gmail.com>
2017-07-19 14:37:24 -0300

    Cambio config a testing

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export default {
   APP_VERSION: 2,
-  API_URL: 'http://172.16.3.100:3002/api/'
+  API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
+  // API_URL: 'http://172.16.3.100:3002/api/'
   // API_URL: 'https://vast-stream-22862.herokuapp.com/api/'
   // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
   // API_URL: 'http://10.1.72.7:27020/api/'
