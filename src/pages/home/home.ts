49d72d93a97d7b3dd4c23915d0a079d76e6c0f15
Mariano Botta <marianoabotta@gmail.com>
2017-08-01 09:31:45 -0300

    Home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,12 +5,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { LoginPage } from '../login/login';
 import { RegistroPersonalDataPage } from '../registro/personal-data/personal-data';
 import { AuthProvider } from '../../providers/auth/auth';
-import { VerificaCodigoPage } from '../verifica-codigo/verifica-codigo';
-import { EscanerDniPage } from '../escaner-dni/escaner-dni';
+
 import { WaitingValidationPage } from '../registro/waiting-validation/waiting-validation';
-// import { WaitingValidationPage } from '../registro/waiting-validation/waiting-validation';
-
-
 
 @Component({
   selector: 'page-home',
@@ -29,16 +25,5 @@ export class HomePage {
 
   login() {
     this.navCtrl.push(LoginPage);
-  }
-
-  registro() {
-    // this.navCtrl.push(RegistroPersonalDataPage);
-    this.navCtrl.push(EscanerDniPage);
-    // this.navCtrl.push(WaitingValidationPage, { user: { nombre: 'Mariano', apellido: 'Botta', documento: 34934522, fechaNacimiento: '2017-01-01', sexo: 'Masculino' } })
-
-  }
-
-  codigo() {
-    this.navCtrl.push(VerificaCodigoPage);
   }
 }
