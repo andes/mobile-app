2fc854d210f5f6884390e1341ca69d96c1a72212
Mariano Botta <marianoabotta@gmail.com>
2017-08-02 15:04:09 -0300

    Farmacias de turno y Feed de noticias

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export default {
   REMEMBER_SESSION: true,
   APP_VERSION: 2,
-  API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
+  // API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
+  API_URL: 'http://172.16.3.30:3002/api/'
 }
