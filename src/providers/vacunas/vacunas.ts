88a2dd9ffca0ddf6f99b666b03b52fc0b67e03af
Mariano Botta <marianoabotta@gmail.com>
2017-08-11 08:39:17 -0300

    Clean variables

--- src/providers/vacunas/vacunas.ts
+++ src/providers/vacunas/vacunas.ts
@@ -1,36 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+
+//providers
+import { NetworkProvider } from './../network';
 
 import config from '../../config';
 
-import { AuthProvider } from '../auth/auth';
-/*
-  Generated class for the VacunasProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class VacunasProvider {
   public user: any;
-  private baseUrl = config.API_URL + 'modules/mobileApp';
+  private baseUrl = 'modules/mobileApp';
 
-  constructor(public http: Http, public authProvider: AuthProvider) {
-    this.user = this.authProvider.user;
+  constructor(
+    public network: NetworkProvider) {
   }
 
   get(params) {
-    return new Promise((resolve, reject) => {
-      let headers = this.authProvider.getHeaders();
-
-      this.http.get(this.baseUrl + '/vacunas', { search: params, headers: headers })
-        .map(res => res.json())
-        .subscribe(data => {
-          resolve(data);
-        }, (err) => {
-          reject(err);
-        });
-    });
+    return this.network.get(this.baseUrl + '/vacunas', params);
   }
 }
