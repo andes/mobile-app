ff4a3b58b98ab9fea4b0c2aa46ab76e7aaa2b433
Luis Parada <luisparadawagner@gmail.com>
2017-08-16 14:05:41 -0300

    Cambio conexión a testing

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export default {
   MAP_KEY: 'AIzaSyAqYUabuhvLRq3c7CYDH6aTu5NHIpTTFQ4',
   // API_URL: 'http://172.16.3.30:3002/api/',
   // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
-  API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
+  API_URL: 'https://d-testing.hospitalneuquen.org.ar/api/'
   // API_URL: 'http://10.1.72.7:27025/api',
 }
