170bf79189cdbea9457c285f365d793e8d8c257d
Luis Parada <luisparadawagner@gmail.com>
2017-08-24 20:10:04 -0300

    Se agrega servicio para traer todas las organizaciones de SISA

--- src/providers/locations/locations.ts
+++ src/providers/locations/locations.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import { Geolocation } from '@ionic-native/geolocation';
+// import { Geolocation } from '@ionic-native/geolocation';
+
+// providers
+import { NetworkProvider } from '../network';
+import config from '../config';
+
 /*
   Generated class for the LocationsProvider provider.
 
@@ -11,39 +16,46 @@ import { Geolocation } from '@ionic-native/geolocation';
 */
 @Injectable()
 export class LocationsProvider {
-  data: any;
+  // data: any;
+  private baseUrl = 'core/tm';
 
-  constructor(private geolocation: Geolocation, public http: Http) { }
+  constructor(public network: NetworkProvider, public http: Http) { }
 
-  load() {
-
-    if (this.data) {
-      return Promise.resolve(this.data);
-    }
-
-    return new Promise(resolve => {
-      // this.geolocation.getCurrentPosition().then((resp) => {
-
-      //   let userLocation = {
-      //     lat: resp.coords.latitude,
-      //     lng: resp.coords.longitude
-      //   };
-
-      this.http.get('assets/data/locations.json').map(res => res.json()).subscribe(data => {
-
-        // this.data = this.applyHaversine(data.locations, userLocation);
-
-        // this.data.sort((locationA, locationB) => {
-        //   return locationA.distance - locationB.distance;
-        // });
-
-        resolve(data.locations);
-      });
-
-      // });
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
+  get() {
+    return this.network.get(this.baseUrl + '/organizacionesCache');
   }
 
-}
+  load() {}
+
+
+
+    //   if (this.data) {
+    //     return Promise.resolve(this.data);
+    //   }
+
+    //   return new Promise(resolve => {
+    //     // this.geolocation.getCurrentPosition().then((resp) => {
+
+    //     //   let userLocation = {
+    //     //     lat: resp.coords.latitude,
+    //     //     lng: resp.coords.longitude
+    //     //   };
+
+    //     this.http.get('assets/data/locations.json').map(res => res.json()).subscribe(data => {
+
+    //       // this.data = this.applyHaversine(data.locations, userLocation);
+
+    //       // this.data.sort((locationA, locationB) => {
+    //       //   return locationA.distance - locationB.distance;
+    //       // });
+
+    //       resolve(data.locations);
+    //     });
+
+    //     // });
+    //   }).catch((error) => {
+    //     console.log('Error getting location', error);
+    //   });
+    // }
+
+  }
