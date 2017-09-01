e4dba228d6179ad56e99c3fa5d05e9f5a3e4a226
Manuel Urbano <manurbano.dev@gmail.com>
2017-09-01 08:58:27 -0300

    Cambiar \ por / en config.xml

--- src/pages/vacunas/vacunas.ts
+++ src/pages/vacunas/vacunas.ts
@@ -14,54 +14,54 @@ import { VacunasProvider } from '../../providers/vacunas/vacunas';
  */
 @IonicPage()
 @Component({
-  selector: 'page-vacunas',
-  templateUrl: 'vacunas.html',
+	selector: 'page-vacunas',
+	templateUrl: 'vacunas.html',
 })
 export class VacunasPage {
-  vacunas: any[] = null;
+	vacunas: any[] = null;
 
-  constructor(
-    public storage: Storage,
-    public vacunasProvider: VacunasProvider,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public authProvider: AuthProvider) {
+	constructor(
+		public storage: Storage,
+		public vacunasProvider: VacunasProvider,
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public authProvider: AuthProvider) {
 
-    this.getVacunas();
-  }
+		this.getVacunas();
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VacunasPage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad VacunasPage');
+	}
 
-  getVacunas() {
-    console.log(this.authProvider);
-    let params = { dni: this.authProvider.user.documento };
+	getVacunas() {
+		console.log(this.authProvider);
+		let params = { dni: this.authProvider.user.documento };
 
-	let promises = [];
-	this.vacunasProvider.getCount(params).then( cantidad => {
-      this.storage.get('cantidadVacunasLocal').then((cantidadVacunasLocal) => {
+		let promises = [];
+		this.vacunasProvider.getCount(params).then(cantidad => {
+			this.storage.get('cantidadVacunasLocal').then((cantidadVacunasLocal) => {
 
-        // buscamos si hay vacunas almacenadas
-        this.storage.get('vacunas').then((vacunasLocal) => {
+				// buscamos si hay vacunas almacenadas
+				this.storage.get('vacunas').then((vacunasLocal) => {
 
-          if (!vacunasLocal || cantidadVacunasLocal != cantidad ) {
-            if (!cantidadVacunasLocal || cantidadVacunasLocal != cantidad) {
-              // almacenamos la cantidad de vacunas en el telefono
-              this.storage.set('cantidadVacunasLocal', cantidad);
-            }
+					if (!vacunasLocal || cantidadVacunasLocal != cantidad) {
+						if (!cantidadVacunasLocal || cantidadVacunasLocal != cantidad) {
+							// almacenamos la cantidad de vacunas en el telefono
+							this.storage.set('cantidadVacunasLocal', cantidad);
+						}
 
-            this.vacunasProvider.get(params).then((data: any[]) => {
-              this.vacunas = data;
-              this.storage.set('vacunas', data);
-            });
+						this.vacunasProvider.get(params).then((data: any[]) => {
+							this.vacunas = data;
+							this.storage.set('vacunas', data);
+						});
 
-          } else {
-            this.vacunas = vacunasLocal;
-          }
-        });
-      });
+					} else {
+						this.vacunas = vacunasLocal;
+					}
+				});
+			});
 
-    });
-  }
+		});
+	}
 }
