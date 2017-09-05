9f1763e08aa5dfcd7f9f5811c632c40ec289a5d8
NicoDino <nicodinolfo@gmail.com>
2017-09-05 10:07:59 -0300

    agrego map key al config para hacer pruebas

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 export default {
   REMEMBER_SESSION: true,
   APP_VERSION: 3,
-  API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
+  MAP_KEY: 'AIzaSyAqYUabuhvLRq3c7CYDH6aTu5NHIpTTFQ4',
+  // API_URL: 'http://localhost:3002/api/',
+  // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
+  API_URL: 'https://d-testing.hospitalneuquen.org.ar/api/'
+  // API_URL: 'http://10.1.72.7:27025/api',
 }
