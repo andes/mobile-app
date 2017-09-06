cd4e711c174961d207a32326f845c2168d25267a
NicoDino <nicodinolfo@gmail.com>
2017-09-06 11:50:02 -0300

    config

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export default {
   MAP_KEY: 'AIzaSyAqYUabuhvLRq3c7CYDH6aTu5NHIpTTFQ4',
   // API_URL: 'http://localhost:3002/api/',
   // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
-  // API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
-  API_URL: 'http://192.168.4.25:3002/api/',
+  // API_URL: 'https://d-testing.hospitalneuquen.org.ar/api/'
+  API_URL: 'http://192.168.0.136:3002/api/',
 
 }
