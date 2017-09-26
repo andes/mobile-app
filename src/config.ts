17b3f9d0c8192d5f5e1be0bc90ee421955c7e229
Luis Parada <luisparadawagner@gmail.com>
2017-09-26 10:50:27 -0300

    Se agrega botón Historia de Salud

--- src/config.ts
+++ src/config.ts
@@ -6,5 +6,5 @@ export default {
   // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
   //API_URL: 'https://d-testing.hospitalneuquen.org.ar/api/'
   // API_URL: 'http://192.168.0.104:3002/api/'
-  API_URL: 'http://192.168.4.8:3002/api/'
+  API_URL: 'http://localhost:3002/api/'
 }
