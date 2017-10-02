4fc678f076d96ddc9732fdc6d95a780b407f5b23
Manuel Urbano <manurbano.dev@gmail.com>
2017-10-02 09:59:20 -0300

    Comienzo funcionalidad reseteo de password

---
+++ src/pages/profile/recuperar-password/recuperar-password.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../../providers/auth/auth';
+import { ToastProvider } from '../../../providers/toast';
+import { FormBuilder, Validators } from '@angular/forms';
+import { PacienteProvider } from '../../../providers/paciente';
+
+
+@Component({
+  selector: 'page-recuperar-password',
+  templateUrl: 'recuperar-password.html',
+})
+export class RecuperarPasswordPage {
+  public formRecuperar: any;
+  public formResetear: any;
+  public displayForm: boolean = false;
+  public reset: any = {};
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public authProvider: AuthProvider,
+    public toast: ToastProvider,
+    public formBuilder: FormBuilder,
+    public pacienteProvider: PacienteProvider) {
+
+    let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+
+    this.formRecuperar = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])]
+    });
+
+    this.formResetear = formBuilder.group({
+      codigo: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      password2: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RecuperarPasswordPage');
+  }
+
+  sendCode() {
+    if (this.formRecuperar) {
+      const email = this.formRecuperar.value.email;
+
+      this.authProvider.sendCode(email).then( result => {
+        this.toast.success("Su codigo ha sido enviado, por favor revise su email");
+      }).catch(error => {
+        console.log(error);
+        this.toast.danger(error.error);
+      });
+    }
+  }
+
+  resetPassword() {
+    if (this.formResetear) {
+      let data: any = {};
+
+      let password = this.formResetear.value.password;
+      let password2 = this.formResetear.value.password2;
+
+      if ( password != password2) {
+        this.toast.danger('INGRESE CORRECTAMENTE LA CONTRASEÑA NUEVA');
+        return;
+      }
+
+      data.password = password;
+      data.old_password = password2;
+
+      this.authProvider.update(data).then((data) => {
+        this.toast.success('DATOS MODIFICADOS CORRECTAMENTE');
+        //this.navCtrl.setRoot(HomePage);
+      }).catch((err) => {
+        if (err.email) {
+          this.toast.danger('EMAIL INCORRECTO');
+        } else {
+          this.toast.danger('CONTRASEÑA ACTUAL INCORRECTA');
+        }
+      });
+    }
+  }
+
+  cancel() {
+    this.displayForm = false,
+    this.reset = {};
+  }
+}
