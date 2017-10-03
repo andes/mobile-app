ad677f11ac1a34c3e6190329243cc939e98e4ef4
Mariano Botta <marianoabotta@gmail.com>
2017-10-03 11:29:30 -0300

    version up

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ export default {
   REMEMBER_SESSION: false,
   APP_VERSION: 4,
   MAP_KEY: 'AIzaSyAqYUabuhvLRq3c7CYDH6aTu5NHIpTTFQ4',
-  // API_URL: 'https://andesapps.hospitalneuquen.org.ar/api/'
+  API_URL: 'https://andesapps.hospitalneuquen.org.ar/api/'
 
-  API_URL: 'http://localhost:3002/api/',
+  // API_URL: 'http://localhost:3002/api/',
   // API_URL: 'http://mongolito.hospitalneuquen.org.ar/api/',
   // API_URL: 'http://d-testing.hospitalneuquen.org.ar/api/'
   // API_URL: 'http://192.168.0.13:3002/api/'
