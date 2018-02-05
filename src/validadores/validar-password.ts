8f25e46f132d9221e467b3c1411f4b4825664b7c
Mariano Botta <marianoabotta@gmail.com>
2018-02-05 11:23:37 -0300

    Pantallas de activación de apps

--- src/validadores/validar-password.ts
+++ src/validadores/validar-password.ts
@@ -2,13 +2,13 @@ import { AbstractControl } from '@angular/forms';
 
 export class PasswordValidation {
 
-  static MatchPassword(AC: AbstractControl) {
-    let password = AC.get('password').value; // to get value in input tag
-    let confirmPassword = AC.get('confirmarPassword').value; // to get value in input tag
-    if (password != confirmPassword) {
-      AC.get('confirmarPassword').setErrors({ MatchPassword: true })
-    } else {
-      return null
+    static MatchPassword(AC: AbstractControl) {
+        let password = AC.get('password').value; // to get value in input tag
+        let confirmPassword = AC.get('confirmarPassword').value; // to get value in input tag
+        if (password != confirmPassword && password && confirmPassword) {
+            AC.get('confirmarPassword').setErrors({ MatchPassword: true })
+        } else {
+            return null
+        }
     }
-  }
 }
