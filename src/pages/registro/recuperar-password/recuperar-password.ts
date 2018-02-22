3938d15a5cb323e488d990e6f77ff66e193e7295
Mariano Botta <marianoabotta@gmail.com>
2018-02-22 10:01:03 -0300

    Estilos varios

--- src/pages/registro/recuperar-password/recuperar-password.ts
+++ src/pages/registro/recuperar-password/recuperar-password.ts
@@ -11,96 +11,113 @@ import { AuthProvider } from '../../../providers/auth/auth';
 import { ToastProvider } from '../../../providers/toast';
 import { PacienteProvider } from '../../../providers/paciente';
 
+let emailRegex = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
 @Component({
-  selector: 'page-recuperar-password',
-  templateUrl: 'recuperar-password.html',
+    selector: 'page-recuperar-password',
+    templateUrl: 'recuperar-password.html',
 })
 export class RecuperarPasswordPage {
-  public formRecuperar: any;
-  public formResetear: any;
-  public displayForm: boolean = false;
-  public reset: any = {};
-  private inProgress = false;
-  @ViewChild(Content) content: Content;
+    public formRecuperar: any;
+    public formResetear: any;
+    public displayForm: boolean = false;
+    public reset: any = {};
+    private inProgress = false;
+    @ViewChild(Content) content: Content;
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public authProvider: AuthProvider,
-    public toast: ToastProvider,
-    public formBuilder: FormBuilder,
-    public pacienteProvider: PacienteProvider) {
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public authProvider: AuthProvider,
+        public toast: ToastProvider,
+        public formBuilder: FormBuilder,
+        public pacienteProvider: PacienteProvider) {
 
-    let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+        let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
 
-    this.formRecuperar = formBuilder.group({
-      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])]
-    });
+        this.formRecuperar = formBuilder.group({
+            email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])]
+        });
 
-    this.formResetear = formBuilder.group({
-      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
-      codigo: ['', Validators.compose([Validators.required])],
-      password: ['', Validators.compose([Validators.required])],
-      password2: ['', Validators.compose([Validators.required])]
-    });
-  }
+        this.formResetear = formBuilder.group({
+            email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+            codigo: ['', Validators.compose([Validators.required])],
+            password: ['', Validators.compose([Validators.required])],
+            password2: ['', Validators.compose([Validators.required])]
+        });
+    }
 
-  ionViewDidLoad() {
-    //
-  }
+    ionViewDidLoad() {
+        //
+    }
 
-  sendCode() {
-    if (this.formRecuperar) {
-      const email = this.formRecuperar.value.email;
-      this.inProgress = true;
-      this.authProvider.resetPassword(email).then( result => {
-        this.inProgress = false;
+    public sendCode() {
+        if (this.formRecuperar && emailRegex.test(this.formRecuperar.value.email)) {
+            const email = this.formRecuperar.value.email;
+            this.inProgress = true;
+            this.authProvider.resetPassword(email).then(result => {
+                this.inProgress = false;
+                this.content.scrollToTop();
+                this.toast.success("Su codigo ha sido enviado, por favor revise su email");
+                this.displayForm = true;
+                this.formResetear.patchValue({ email: email });
+            }).catch(error => {
+                this.inProgress = false;
+                if (error) {
+                    this.toast.danger(error.error);
+                }
+            });
+        }
+    }
+
+    public yaTengo() {
+        this.displayForm = true;
         this.content.scrollToTop();
-        this.toast.success("Su codigo ha sido enviado, por favor revise su email");
-        this.displayForm= true;
-        this.formResetear.patchValue({email: email});
-      }).catch(error => {
-        this.inProgress = false;
-        if (error) {
-          this.toast.danger(error.error);
-        }
-      });
     }
-  }
 
-  yaTengo() {
-    this.displayForm= true;
-    this.content.scrollToTop();
-  }
+    public resetPassword() {
+        if (this.formResetear && !this.formResetear.invalid) {
 
-  resetPassword() {
-    if (this.formResetear) {
+            let email = this.formResetear.value.email;
+            let codigo = this.formResetear.value.codigo;
+            let password = this.formResetear.value.password;
+            let password2 = this.formResetear.value.password2;
 
-      let email = this.formResetear.value.email;
-      let codigo = this.formResetear.value.codigo;
-      let password = this.formResetear.value.password;
-      let password2 = this.formResetear.value.password2;
-
-      if ( password != password2) {
-        this.toast.danger('LAS CONTRASEÑAS NO COINCIDEN');
-        return;
-      }
-      this.inProgress = true;
-      this.authProvider.restorePassword(email, codigo, password, password2).then((data) => {
-        this.inProgress = false;
-        this.toast.success('PASSWORD MODIFICADO CORRECTAMENTE');
-        this.navCtrl.setRoot(HomePage);
-      }).catch((err) => {
-        this.inProgress = false;
-        if (err) {
-          this.toast.danger(err.error);
+            if (password != password2) {
+                this.toast.danger('LAS CONTRASEÑAS NO COINCIDEN');
+                return;
+            }
+            this.inProgress = true;
+            this.authProvider.restorePassword(email, codigo, password, password2).then((data) => {
+                this.inProgress = false;
+                this.toast.success('PASSWORD MODIFICADO CORRECTAMENTE');
+                this.navCtrl.setRoot(HomePage);
+            }).catch((err) => {
+                this.inProgress = false;
+                if (err) {
+                    this.toast.danger(err.error);
+                }
+            });
         }
-      });
     }
-  }
 
-  cancel() {
-    this.displayForm = false,
-    this.reset = {};
-  }
+    public cancel() {
+        this.displayForm = false,
+            this.reset = {};
+    }
+
+    public onKeyPress($event, tag) {
+        if ($event.keyCode == 13) {
+            if (tag === 'submit-1') {
+                this.sendCode();
+            } else if (tag === 'submit-2') {
+                this.resetPassword();
+            } else {
+                let element = document.getElementById(tag);
+                if (element) {
+                    element.focus();
+                }
+            }
+        }
+    }
+
 }
