fad3338f91099510e7fb3394ab37a346cfdebb80
Mariano Botta <marianoabotta@gmail.com>
2018-02-22 08:59:20 -0300

    Datos de acceso

--- src/pages/registro/verifica-codigo/verifica-codigo.ts
+++ src/pages/registro/verifica-codigo/verifica-codigo.ts
@@ -13,74 +13,73 @@ import { ToastProvider } from '../../../providers/toast';
 import { DeviceProvider } from '../../../providers/auth/device';
 
 @Component({
-  selector: 'page-verifica-codigo',
-  templateUrl: 'verifica-codigo.html',
+    selector: 'page-verifica-codigo',
+    templateUrl: 'verifica-codigo.html',
 })
 export class VerificaCodigoPage {
-  mostrarMenu: boolean = false;
-  formIngresoCodigo: FormGroup;
-  submit: boolean = false;
-  email: any = '';
-  codigo: string;
-  emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
-  running = false;
+    mostrarMenu: boolean = false;
+    formIngresoCodigo: FormGroup;
+    submit: boolean = false;
+    email: any = '';
+    codigo: string;
+    emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+    running = false;
 
-  constructor(
-    public toastProvider: ToastProvider,
-    public alertCtrl: AlertController,
-    public storage: Storage,
-    public authService: AuthProvider,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public formBuilder: FormBuilder,
-    public deviceProvider: DeviceProvider) {
+    constructor(
+        public toastProvider: ToastProvider,
+        public alertCtrl: AlertController,
+        public storage: Storage,
+        public authService: AuthProvider,
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public formBuilder: FormBuilder,
+        public deviceProvider: DeviceProvider) {
 
-    this.formIngresoCodigo = formBuilder.group({
-      codigo: ['', Validators.compose([Validators.required, Validators.maxLength(6)])],
-      email: ['', Validators.compose([Validators.required, Validators.pattern(this.emailRegex)])],
-      // codigo: ['']
-    });
+        this.formIngresoCodigo = formBuilder.group({
+            codigo: ['', Validators.compose([Validators.required, Validators.maxLength(6)])],
+            email: ['', Validators.compose([Validators.required, Validators.pattern(this.emailRegex)])],
+            // codigo: ['']
+        });
 
-  }
+    }
 
-  ngOnDestroy() {
-  }
+    ngOnDestroy() {
+    }
 
-  ionViewDidLoad() {
-  }
+    ionViewDidLoad() {
+    }
 
-  codeTostring(code) {
-      let c = String(code);
-      while (c.length < 6) { c = '0' + c};
-      return c;
-  }
+    codeTostring(code) {
+        let c = String(code);
+        while (c.length < 6) { c = '0' + c };
+        return c;
+    }
 
-  onSubmit({ value, valid }: { value: any, valid: boolean }) {
-    this.running = true;
-    this.authService.checkCode(value.email, value.codigo).then(() => {
-      this.running = false;
-      this.navCtrl.push(RegistroUserDataPage, {email: value.email, code: this.codeTostring(value.codigo)});
+    onSubmit({ value, valid }: { value: any, valid: boolean }) {
+        this.running = true;
+        this.authService.checkCode(value.email, value.codigo).then(() => {
+            this.running = false;
+            this.navCtrl.push(RegistroUserDataPage, { email: value.email, code: this.codeTostring(value.codigo) });
 
-    }).catch((err) => {
-      this.running = false;
-        if (err) {
-          if (err.message === 'email existente') {
-            this.toastProvider.danger('EL EMAIL YA ESTA EN USO');
-          } else {
-            this.toastProvider.danger('CODIGO INCORRECTO O EXPIRADO');
-          }
-        }
-    });
+        }).catch((err) => {
+            this.running = false;
+            if (err) {
+                if (err.message === 'email existente') {
+                    this.toastProvider.danger('EL EMAIL YA ESTA EN USO');
+                } else {
+                    this.toastProvider.danger('CODIGO INCORRECTO O EXPIRADO');
+                }
+            }
+        });
+    }
 
-  }
-
-  showAlert(title: string, text: string) {
-    let alert = this.alertCtrl.create({
-      title: '',
-      subTitle: text,
-      buttons: ['OK']
-    });
-    alert.present();
-  }
+    showAlert(title: string, text: string) {
+        let alert = this.alertCtrl.create({
+            title: '',
+            subTitle: text,
+            buttons: ['OK']
+        });
+        alert.present();
+    }
 
 }
