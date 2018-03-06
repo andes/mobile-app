34d6507f97dd03df202f1c206f81d0525f9cc018
Mariano Botta <marianoabotta@gmail.com>
2018-03-06 13:53:44 -0300

    cabios profile

--- src/pages/profile/paciente/tab-view-profile.ts
+++ src/pages/profile/paciente/tab-view-profile.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ProfilePacientePage } from './profile-paciente';
 import { DondeVivoDondeTrabajoPage } from './donde-vivo-donde-trabajo/donde-vivo-donde-trabajo';
 import { ProfileContactosPage } from './profile-contactos';
+import { ErrorReporterProvider } from '../../../providers/errorReporter';
 /**
  * Generated class for the CentrosSaludPage page.
  *
@@ -16,14 +17,21 @@ import { ProfileContactosPage } from './profile-contactos';
 })
 export class TabViewProfilePage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public reporter: ErrorReporterProvider) {
     }
 
     ionViewDidLoad() {
-        //
+        this.reporter.alert();
     }
 
     tab1Root: any = ProfilePacientePage;
     tab2Root: any = DondeVivoDondeTrabajoPage;
     tab3Root: any = ProfileContactosPage;
+
+    onBugReport() {
+        this.reporter.report();
+    }
 }
