6b297a00618b7877332765ae439973b262946d3f
Hugo <hfernandez@neuquen.gov.ar>
2018-10-18 10:31:21 -0300

    add loading

--- src/pages/datos-utiles/campanias/campanias-list.ts
+++ src/pages/datos-utiles/campanias/campanias-list.ts
@@ -13,7 +13,7 @@ import { CampaniasProvider } from '../../../providers/campanias';
     templateUrl: 'campanias-list.html'
 })
 export class CampaniasListPage {
-    campanias: any = [];
+    campanias = null;
     constructor(
         public navCtrl: NavController,
         private campaniasProvider: CampaniasProvider,
