06f523e76b78b9a8297497e4fc36281eb4e5ee80
Nahir <nahirsaddi@gmail.com>
2019-06-12 12:36:30 -0300

    iconos y borrado

--- src/pages/gestion/monitoreo.ts
+++ src/pages/gestion/monitoreo.ts
@@ -18,7 +18,7 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 
 export class MonitoreoComponent implements OnInit {
-    formularioUsuario: FormGroup;
+    public form: FormGroup;
     public _attachment: any = [];
     public imagen: string = null;
     public correos: any = [];
@@ -26,35 +26,38 @@ export class MonitoreoComponent implements OnInit {
     public asunto: string;
     public mensaje: string;
     constructor(public navCtrl: NavController,
-        private alertCtrl: AlertController,
-        private fb: FormBuilder,
+        private _FORM: FormBuilder,
         private _CAMERA: Camera,
         public toast: ToastProvider,
         public emailCtr: EmailComposer,
         public authService: AuthProvider,
 
     ) {
+        this.form = this._FORM.group({
+            'to': ['', Validators.required],
+            'subject': ['', Validators.required],
+            'message': ['', Validators.required]
+
+        });
     }
 
     ngOnInit() {
-        this.correos = ['nahirsaddi@gmail.com', 'silviroa@gmail.com', 'celeste.carolina.s@gmail.com']
+        this.correos = ['nahirsaddi@gmail.com', 'silviroa@gmail.com', 'celeste.carolina.s@gmail.com', 'marcosavino19@gmail.com ']
     }
-
 
     tomarFoto() {
         let options: CameraOptions = {
             destinationType: this._CAMERA.DestinationType.DATA_URL,
             correctOrientation: true
-        } as CameraOptions;
+        };
 
         // sacamos la foto
-        this._CAMERA.getPicture(options).then((imageData) => {
+        this._CAMERA.getPicture(options).then((imageData: any) => {
             this.imagen = `data:image/jpeg;base64,${imageData}`;
             this._attachment.push(this.imagen);
-        })
-            .catch(error => {
-                this.toast.danger('Error al sacar la foto.');
-            });
+        }).catch(error => {
+            this.toast.danger('Error al sacar la foto.');
+        });
     }
 
     seleccionarArchivo() {
@@ -77,8 +80,20 @@ export class MonitoreoComponent implements OnInit {
     }
 
     async armarCorreo() {
-        if (this.to.length > 0 && this.asunto && this.mensaje && this._attachment.length > 0) {
-            await this.authService.enviarCorreo(this.to, this.asunto, this.mensaje, this._attachment);
+        let to = this.form.controls['to'].value,
+            subject: string = 'ANDES - ' + this.form.controls['subject'].value,
+            message: string = this.form.controls['message'].value;
+
+        if (this._attachment.length > 0) {
+            await this.authService.enviarCorreo(to, subject, message, this._attachment);
+
+        } else {
+            this.toast.danger('Falta adjuntar archivo');
+        }
+    }
+    delete(item) {
+        if (this._attachment.length > 0) {
+            this._attachment.splice(item, 1);
         }
     }
 }
