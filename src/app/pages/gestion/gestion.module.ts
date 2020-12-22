79057c518edf9abb48747622fb1f4c92b7cfc96c
Andrés Boc-Ho <andresin@gmail.com>
2020-12-22 11:01:52 -0300

    fix: mapa gestión

--- src/app/pages/gestion/gestion.module.ts
+++ src/app/pages/gestion/gestion.module.ts
@@ -1,3 +1,4 @@
+import { AdsModule } from './../../ads/ads.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
