4bd46d37ddc9f73c38117404d29631e51954abcf
Juliana <juli.otero01@gmail.com>
2021-01-06 11:44:01 -0300

    refactor(login): arreglo de pantalla login y limpieza de código

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,23 +11,26 @@ import { DisclaimersProvider } from 'src/providers/auth/disclaimer';
 import { DisclaimerPage } from './disclaimers/accept-disclaimer';
 import { OrganizacionesPage } from './organizaciones/organizaciones';
 import { AdsModule } from 'src/app/ads/ads.module';
+import { RecuperarPasswordPage } from './recuperar-password/recuperar-password';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoginPageRoutingModule,
-    AdsModule
-  ],
-  declarations: [
-    DisclaimerPage,
-    LoginPage,
-    OrganizacionesPage
-  ],
-  providers: [
-    DisclaimersProvider
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        IonicModule,
+        LoginPageRoutingModule,
+        AdsModule
+    ],
+    declarations: [
+        DisclaimerPage,
+        LoginPage,
+        OrganizacionesPage,
+        RecuperarPasswordPage
+    ],
+    providers: [
+        DisclaimersProvider
+    ],
 
 })
 export class LoginPageModule { }
--- src/app/pages/login/recuperar-password/recuperar-password.ts
+++ src/app/pages/login/recuperar-password/recuperar-password.ts
@@ -1,15 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
 import { FormBuilder, Validators } from '@angular/forms';
 import { IonContent } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 // Providers
 import { AuthProvider } from '../../../../providers/auth/auth';
 import { ToastProvider } from '../../../../providers/toast';
-import { PacienteProvider } from '../../../../providers/paciente';
-import { Router } from '@angular/router';
 
-let _emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 @Component({
     selector: 'app-recuperar-password',
     templateUrl: 'recuperar-password.html',
@@ -19,24 +15,19 @@ export class RecuperarPasswordPage {
     public formResetear: any;
     public displayForm = false;
     public reset: any = {};
-    private inProgress = false;
+    public inProgress = false;
     @ViewChild(IonContent) content: IonContent;
 
     constructor(
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        public authProvider: AuthProvider,
-        public toast: ToastProvider,
-        public formBuilder: FormBuilder,
-        public pacienteProvider: PacienteProvider,
-        public router: Router) {
+        private authProvider: AuthProvider,
+        private toast: ToastProvider,
+        private formBuilder: FormBuilder,
+        private router: Router) {
 
         const emailRegex = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
-
         this.formRecuperar = formBuilder.group({
             email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])]
         });
-
         this.formResetear = formBuilder.group({
             email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
             codigo: ['', Validators.compose([Validators.required])],
@@ -45,28 +36,21 @@ export class RecuperarPasswordPage {
         });
     }
 
-    ionViewDidLoad() {
-        //
-    }
-
     public sendCode() {
-        console.log('sendCode');
-        if (this.formRecuperar && _emailRegex.test(this.formRecuperar.value.email)) {
-            const email = this.formRecuperar.value.email;
-            this.inProgress = true;
-            this.authProvider.resetPassword(email).then(result => {
-                this.inProgress = false;
-                this.content.scrollToTop();
-                this.toast.success('EL CODIGO FUE ENVIADO');
-                this.displayForm = true;
-                this.formResetear.patchValue({ email });
-            }).catch(error => {
-                this.inProgress = false;
-                if (error) {
-                    this.toast.danger(error.error);
-                }
-            });
-        }
+        const email = this.formRecuperar.value.email;
+        this.inProgress = true;
+        this.authProvider.resetPassword(email).then(result => {
+            this.inProgress = false;
+            this.content.scrollToTop();
+            this.toast.success('EL CODIGO FUE ENVIADO');
+            this.displayForm = true;
+            this.formResetear.patchValue({ email });
+        }).catch(error => {
+            this.inProgress = false;
+            if (error) {
+                this.toast.danger(error.error);
+            }
+        });
     }
 
     public yaTengo() {
@@ -75,29 +59,25 @@ export class RecuperarPasswordPage {
     }
 
     public resetPassword() {
-        if (this.formResetear && !this.formResetear.invalid) {
-
-            const email = this.formResetear.value.email;
-            const codigo = this.formResetear.value.codigo;
-            const password = this.formResetear.value.password;
-            const password2 = this.formResetear.value.password2;
-
-            if (password !== password2) {
-                this.toast.danger('LAS CONTRASEÑAS NO COINCIDEN');
-                return;
-            }
-            this.inProgress = true;
-            this.authProvider.restorePassword(email, codigo, password, password2).then((data) => {
-                this.inProgress = false;
-                this.toast.success('PASSWORD MODIFICADO CORRECTAMENTE');
-                this.router.navigate(['home']);
-            }).catch((err) => {
-                this.inProgress = false;
-                if (err) {
-                    this.toast.danger(err.error);
-                }
-            });
+        const email = this.formResetear.value.email;
+        const codigo = this.formResetear.value.codigo;
+        const password = this.formResetear.value.password;
+        const password2 = this.formResetear.value.password2;
+        if (password !== password2) {
+            this.toast.danger('LAS CONTRASEÑAS NO COINCIDEN');
+            return;
         }
+        this.inProgress = true;
+        this.authProvider.restorePassword(email, codigo, password, password2).then((data) => {
+            this.inProgress = false;
+            this.toast.success('PASSWORD MODIFICADO CORRECTAMENTE');
+            this.router.navigate(['home']);
+        }).catch((err) => {
+            this.inProgress = false;
+            if (err) {
+                this.toast.danger(err.error);
+            }
+        });
     }
 
     public cancel() {
@@ -108,9 +88,17 @@ export class RecuperarPasswordPage {
     public onKeyPress($event, tag) {
         if ($event.keyCode === 13) {
             if (tag === 'submit-1') {
-                this.sendCode();
+                if (this.formRecuperar && this.formRecuperar.valid) {
+                    this.sendCode();
+                } else {
+                    this.toast.danger('Revise los datos ingresados');
+                }
             } else if (tag === 'submit-2') {
-                this.resetPassword();
+                if (this.formResetear && this.formResetear.valid) {
+                    this.resetPassword();
+                } else {
+                    this.toast.danger('Revise los datos ingresados');
+                }
             } else {
                 const element = document.getElementById(tag);
                 if (element) {
