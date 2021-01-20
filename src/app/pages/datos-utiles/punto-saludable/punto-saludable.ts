c3f8da4191a1d73e84b7eb659ee14f7ff3ff581f
Juliana <juli.otero01@gmail.com>
2021-01-20 11:55:14 -0300

    feat(punto-saludable): scroll infinito

--- src/app/pages/datos-utiles/punto-saludable/punto-saludable.ts
+++ src/app/pages/datos-utiles/punto-saludable/punto-saludable.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as moment from 'moment';
 import { NoticiasProvider } from 'src/providers/noticias';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
     selector: 'app-punto-saludable',
@@ -9,6 +10,9 @@ import { NoticiasProvider } from 'src/providers/noticias';
 export class PuntoSaludablePage implements OnInit {
     noticias: any = [];
     loading = true;
+    count = 15;
+    max_id;
+    @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
     constructor(
         private noticiasProvider: NoticiasProvider) {
@@ -22,10 +26,31 @@ export class PuntoSaludablePage implements OnInit {
 
     ngOnInit() {
         moment.locale('es');
-        this.noticiasProvider.puntoSaludable({}).then((data) => {
+        this.noticiasProvider.puntoSaludable({ count: this.count }).then((data: any) => {
             this.noticias = data;
+            this.max_id = data[data.length - 1].id;
             this.loading = false;
+            if (data.length < this.count) {
+                this.infiniteScroll.disabled = true;
+            }
         });
+    }
+
+    loadData(event) {
+        setTimeout(() => {
+            event.target.complete();
+            this.noticiasProvider.puntoSaludable({ count: this.count, max_id: this.max_id }).then((data: any) => {
+                this.noticias = this.noticias.concat(data.slice(1));
+                this.max_id = data[data.length - 1].id;
+                if (data.length < this.count) {
+                    event.target.disabled = true;
+                }
+            });
+        }, 500);
+    }
+
+    toggleInfiniteScroll() {
+        this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
     }
 
 }
