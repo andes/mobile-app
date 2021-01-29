e27475f07653f52c6c808c97daa1376180520570
Andrés Boc-Ho <andresin@gmail.com>
2021-01-29 15:36:50 -0300

    chore: Observables

--- src/providers/geo-provider.ts
+++ src/providers/geo-provider.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Platform } from '@ionic/angular';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 @Injectable()
 export class GeoProvider {
     public actualPosition = null;
