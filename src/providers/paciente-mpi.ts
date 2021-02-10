65e487031961e16eee6b47872fe58cd8f5ddf7f8
Juliana <juli.otero01@gmail.com>
2021-02-10 13:48:35 -0300

    fix(mpi): arreglo al escanear un dni como profesional

--- src/providers/paciente-mpi.ts
+++ src/providers/paciente-mpi.ts
@@ -6,26 +6,21 @@ import { NetworkProvider } from './network';
 export class PacienteMPIService {
 
     constructor(private network: NetworkProvider) { }
-    private pacienteUrl = 'core/mpi/pacientes';  // URL to web api
+    private pacienteUrl = 'core-v2/mpi/pacientes';  // URL to web api
 
     get(params) {
         return this.network.get(this.pacienteUrl, params);
     }
 
-    /**
-     * Metodo getById. Trae un objeto paciente por su Id.
-     * @param  id Busca por Id
-     */
     getById(id: string) {
         return this.network.get(this.pacienteUrl + '/' + id, {});
     }
 
     save(paciente) {
         if (paciente.id) {
-            return this.network.put(this.pacienteUrl + '/' + paciente.id, paciente);
+            return this.network.patch(this.pacienteUrl + '/' + paciente.id, paciente);
         } else {
             return this.network.post(this.pacienteUrl, paciente);
-
         }
     }
 
