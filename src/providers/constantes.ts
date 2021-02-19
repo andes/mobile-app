ad612fdb19091b7345a7da55edd20f6bdb8a8f69
Juliana <juli.otero01@gmail.com>
2021-02-19 12:34:33 -0300

    feat(am): permitir al profesional cambiar de organización

--- src/providers/constantes.ts
+++ src/providers/constantes.ts
@@ -5,32 +5,28 @@ import { NetworkProvider } from './network';
 
 @Injectable()
 export class ConstanteProvider {
-  public organizaciones: any;
+    public organizaciones: any;
 
-  private baseUrl = 'core/tm';
-  private authUrl = 'auth';
+    private baseUrl = 'core/tm';
+    private authUrl = 'auth';
 
-  constructor(
-    public network: NetworkProvider
-  ) {
-    // this.user = this.auth.user;
-  }
-
-  provincias() {
-    return this.network.get(this.baseUrl + '/provincias', {});
-  }
-
-  localidades(filter) {
-    return this.network.get(this.baseUrl + '/localidades', filter);
-  }
-
-  getOrganizaciones(usuario) {
-    const params: any = {};
-    if (usuario) {
-      params.usuario = usuario;
+    constructor(
+        public network: NetworkProvider
+    ) {
+        // this.user = this.auth.user;
     }
-    return this.network.get(this.authUrl + '/organizaciones', params);
-  }
+
+    provincias() {
+        return this.network.get(this.baseUrl + '/provincias', {});
+    }
+
+    localidades(filter) {
+        return this.network.get(this.baseUrl + '/localidades', filter);
+    }
+
+    getOrganizaciones() {
+        return this.network.get(this.authUrl + '/organizaciones', {});
+    }
 
 
 
