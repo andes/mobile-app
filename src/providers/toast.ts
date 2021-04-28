2cbc23f8d29e2367d774ee8ab937c47eca8cab73
Andrés Boc-Ho <andresin@gmail.com>
2021-04-28 11:23:09 -0300

    refactor(toast): se actualiza para Ionic5

--- src/providers/toast.ts
+++ src/providers/toast.ts
@@ -8,12 +8,12 @@ export class ToastProvider {
         //
     }
 
-    async displayToast(title, type = 'success', time = 3000, callbak = null) {
+    async displayToast(title, type = 'success', time = 3000) {
         const toast = await this.toastCtrl.create({
             message: title,
             duration: time,
             position: 'bottom',
-            cssClass: type
+            color: type
         });
         toast.present();
         const { role } = await toast.onDidDismiss();
@@ -28,12 +28,12 @@ export class ToastProvider {
         }
     }
 
-    public async success(text, duration = 3000, callbak = null) {
-        this.displayToast(text, 'success', duration, callbak);
+    public async success(text, duration = 3000) {
+        this.displayToast(text, 'success', duration);
     }
 
-    public async danger(text, duration = 3000, callbak = null) {
-        this.displayToast(text, 'danger', duration, callbak);
+    public async danger(text, duration = 3000) {
+        this.displayToast(text, 'danger', duration);
     }
 
 
