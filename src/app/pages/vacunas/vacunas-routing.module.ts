03ff6a14f2e25e4718d76bea8beb5777bc4128ff
palita1991 <marianopalavecino7@gmail.com>
2021-08-09 12:15:43 -0300

    feat(MOBILE): agrega detalle de vacunacion

--- src/app/pages/vacunas/vacunas-routing.module.ts
+++ src/app/pages/vacunas/vacunas-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VacunasDetallePage } from './vacunas-detalle.page';
 import { VacunasPage } from './vacunas.page';
 
 const routes: Routes = [
     {
         path: '',
         component: VacunasPage
+    },
+    {
+        path: 'vacunas-detalle',
+        component: VacunasDetallePage
     }
 ];
 
