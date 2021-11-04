5382a04119917a2790a86944109a0ae86a721b31
Andrés Boc-Ho <andresin@gmail.com>
2021-11-04 15:29:39 -0300

    feat(mpi): se reestablece scan

--- src/app/pages/profesional/mpi/scan-documento/scan-documento.ts
+++ src/app/pages/profesional/mpi/scan-documento/scan-documento.ts
@@ -1,11 +1,10 @@
 import { AlertController } from '@ionic/angular';
 import { Component } from '@angular/core';
-// import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-// providers
-// import { ScanParser } from 'src/providers/scan-parser';
-// import { ToastProvider } from 'src/providers/toast';
-// import { Router } from '@angular/router';
-// import * as configScan from 'src/providers/config-scan';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { ScanParser } from 'src/providers/scan-parser';
+import { ToastProvider } from 'src/providers/toast';
+import { Router } from '@angular/router';
+import * as configScan from 'src/providers/config-scan';
 
 @Component({
     selector: 'app-scan-documento',
@@ -20,18 +19,34 @@ export class ScanDocumentoPage {
     public textoLibre: string = null;
 
     constructor(
-        // private router: Router,
-        // private barcodeScanner: BarcodeScanner,
-        // private scanParser: ScanParser,
-        // private toastCtrl: ToastProvider,
+        private router: Router,
+        private barcodeScanner: BarcodeScanner,
+        private scanParser: ScanParser,
+        private toastCtrl: ToastProvider,
         private alertCtrl: AlertController) {
     }
 
-    async scanner() {
+    scanner() {
 
+        const options = configScan.setOptions();
+        this.barcodeScanner.scan(options).then((barcodeData) => {
+            const datos = this.scanParser.scan(barcodeData.text);
+            if (datos) {
+                this.router.navigate(['profesional/registro-paciente'],
+                    { queryParams: { datos: JSON.stringify(datos), scan: barcodeData.text } });
+            } else {
+                this.toastCtrl.danger('Documento inválido.');
+            }
+
+        }, (err) => {
+            this.scanFail(err);
+        });
+    }
+
+    async scanFail(error) {
         const alert = await this.alertCtrl.create({
-            header: 'Registro deshabilitado temporalmente',
-            message: 'Por problemas con RENAPER el registro de pacientes nuevos está deshabilitado temporalmente.',
+            header: 'No se pudo registrar al paciente',
+            message: 'Hubo un problema al escanear el Documento. Por favor intente nuevamente.',
             buttons: [
                 {
                     text: 'Aceptar',
@@ -41,19 +56,5 @@ export class ScanDocumentoPage {
         });
 
         await alert.present();
-
-        // const options = configScan.setOptions();
-        // this.barcodeScanner.scan(options).then((barcodeData) => {
-        //     const datos = this.scanParser.scan(barcodeData.text);
-        //     if (datos) {
-        //         this.router.navigate(['profesional/registro-paciente'],
-        //             { queryParams: { datos: JSON.stringify(datos), scan: barcodeData.text } });
-        //     } else {
-        //         this.toastCtrl.danger('Documento inválido.');
-        //     }
-
-        // }, (err) => {
-
-        // });
     }
 }
