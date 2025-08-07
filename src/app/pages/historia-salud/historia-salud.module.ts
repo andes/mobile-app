11f091b8bb83ba1225523d5e8aace016f3f80b7d
juliotero <juli.otero01@gmail.com>
2025-08-07 15:36:27 -0300

    AM - implementacion de visualización de recetas (#227)
    * feat: implementacion de visualización de recetas
    
    * fix: Control sobre indicaciones en recetas
    
    ---------
    
    Co-authored-by: Juliana Otero <julianaotero@MacBook-Air-de-Juliana.local>
    Co-authored-by: silviroa <silviroa@gmail.com>

--- src/app/pages/historia-salud/historia-salud.module.ts
+++ src/app/pages/historia-salud/historia-salud.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AdsModule } from 'src/app/ads/ads.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,20 +9,24 @@ import { HistoriaSaludPageRoutingModule } from './historia-salud-routing.module'
 
 import { HistoriaSaludPage } from './historia-salud.page';
 import { CategoriasProvider } from 'src/providers/historia-salud/categorias';
+import { RecetasProvider } from 'src/providers/historia-salud/recetas';
 import { DetalleCategoriaPage } from './categorias/detalle-categoria';
+import { RecetasPage } from './recetas/recetas';
+import { DetalleRecetaPage } from './recetas/detalle-receta/detalle-receta';
 @NgModule({
     imports: [
+        AdsModule,
         CommonModule,
         FormsModule,
         IonicModule,
-        HistoriaSaludPageRoutingModule
+        HistoriaSaludPageRoutingModule,
     ],
     declarations: [
         HistoriaSaludPage,
-        DetalleCategoriaPage
+        DetalleCategoriaPage,
+        RecetasPage,
+        DetalleRecetaPage,
     ],
-    providers: [
-        CategoriasProvider
-    ]
+    providers: [CategoriasProvider, RecetasProvider],
 })
 export class HistoriaSaludPageModule {}
